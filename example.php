fb09c2b21c40d075edef098643112a4abc15eeb1
Rui Pedro <bitkill@users.noreply.github.com>
2014-10-02 16:25:49 +0100

    Update example.php

--- example.php
+++ example.php
@@ -4,12 +4,8 @@ include_once ('socket.io.php');
 
 //Create a socket.io connection and send a simple message
 $socketio = new SocketIO();
-if ($socketio->send('localhost', 9090, 'Hello world!')){
+if ($socketio->send('localhost', 9090, 'message', 'Hello world!')){
     echo 'we sent the message and disconnected';
 } else {
     echo 'Sorry, we have a mistake :\'(';
 }
-
-
-
-?>
